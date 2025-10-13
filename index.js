e73b66a14955b8d69bcf63056593d71d9364fd67
ramzy-ai <rameyabohane2000@gmail.com>
2025-10-13 16:43:31 +0300

    13102025

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function goToForgotPage() {
   // זה יוביל אותך לעמוד חדש (לדוגמה forgot.html)
   window.location.href = "sign.html";
 }
+function goToForgotSign(){
+  window.location.href = "index.html";
+}
+
+const pwd = document.getElementById('password');
+const chk = document.getElementById('showPassword');
+
+ chk.addEventListener('change', () => {
+ pwd.type = chk.checked ? 'text' : 'password';
+ });
