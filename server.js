a6e4e10e42f32e00bc1265aabfe1776706bb2b29
ramzy-ai <rameyabohane2000@gmail.com>
2025-11-22 13:44:13 +0200

    22112025

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const bcrypt  = require('bcrypt');
 
 const app = express();
-const port = 3019;
+const port = process.env.PORT || 3019; 
 
 app.use(cors());
 app.use(express.json());
